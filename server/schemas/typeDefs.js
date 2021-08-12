b2445704e05aeec4048699e5b13287e6da1e6d32
Zak Stamps <theoneandonlyzako@gmail.com>
2021-08-12 12:36:23 -0700

    added schemas according to user-controllers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Query {
+    me: User
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookBody: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
