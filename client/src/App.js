cdf690f54a18bae63f05324329efcb7f464aa06e
Zak Stamps <theoneandonlyzako@gmail.com>
2021-08-12 17:05:15 -0700

    replaced BrowserRouter with HashRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
